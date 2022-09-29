f2ce7f0360dfcf7d457b1616b2e266d21aef30e5
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-29 11:02:49 -0500

    Adding solution square file

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""
+Script that print square
+"""
+
 
 class square():
-
+    """
+    Class square
+    """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """
+        method initialized
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -14,9 +23,15 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """
+        method perimeter of the square
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        method str
+        """
         return "{}/{}".format(self.width, self.height)
 
 
