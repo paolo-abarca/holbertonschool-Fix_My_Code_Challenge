e0656b8eaf2924c574b4e9a354a53e0763e0706b
paolo-abarca <pao.lojag_1999@hotmail.com>
2022-09-29 11:13:50 -0500

    Adding solution square poo

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -18,11 +18,11 @@ class Square():
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
         """
         method perimeter of the square
         """
@@ -41,5 +41,5 @@ if __name__ == "__main__":
     """
     s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.perimeter())
